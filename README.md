fea95b60be1ec2def103686c1a8e9cf46e3dd0c9
Franco Ortega <francopdx@gmail.com>
2021-08-31 09:08:49 -0700

    install eslint

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # demo-react-01-todos
 Todo List demo built with React.
+
+**Dependencies**
+1. npx create-react-app
+1. npm install eslint --save-dev
+1. npm install eslint-plugin-react --save-dev
+1. npx eslint --init
--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders App with "Hello World"', () => {
-  render(<App />);
-  const element = screen.getByText(/hello world/i);
-  expect(element).toBeInTheDocument();
+describe('App test', () => {
+  test('renders App with "Hello World"', () => {
+    render(<App />);
+    const element = screen.getByText(/hello world/i);
+    expect(element).toBeInTheDocument();
+  });
 });
+
